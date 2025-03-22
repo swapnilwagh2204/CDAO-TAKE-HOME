b95be371daa4c207ff922b68532ef5f9e750b636
swapnilwagh <swapnilwagh@swapnilwaghs-MacBook-Air.local>
2025-03-21 18:11:02 -0700

    nit fix

---
+++ tests/conftest.py
@@ -0,0 +1,10 @@
+import pytest
+from flask_apis import app
+
+
+@pytest.fixture
+def client():
+    """Create a test client for the Flask app."""
+    app.config["TESTING"] = True
+    with app.test_client() as client:
+        yield client
